07a7650bdd60468490729d7238b0b891ede16165
Ivan Kalugin <42765419+iklove2@users.noreply.github.com>
2024-01-02 23:10:12 +0300

    json и сверстан верх

---
+++ js/shopmenu.js
@@ -0,0 +1,158 @@
+const dataShops = `[
+  { 
+    "img" : "url(img/shopmenu/page1.png)",
+    "title" : "milk pitcher",
+    "text" : "page 1",
+    "ahref" : "page1.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page2.png)",
+    "title" : "milk pitcher",
+    "text" : "page 2",
+    "ahref" : "page2.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page3.png)",
+    "title" : "coffee pour over kettle",
+    "text" : "page 3",
+    "ahref" : "page3.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page4.png)",
+    "title" : "coffee pour over kettle",
+    "text" : "page 4",
+    "ahref" : "page4.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page5.png)",
+    "title" : "electrical coffee pour over kettle",
+    "text" : "page 5",
+    "ahref" : "page5.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page6.png)",
+    "title" : "coffee tamper",
+    "text" : "page 6",
+    "ahref" : "page6.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page7.png)",
+    "title" : "coffee tamper",
+    "text" : "page 7",
+    "ahref" : "page7.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page8.png)",
+    "title" : "coffee distributor",
+    "text" : "page 8",
+    "ahref" : "page8.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page9.png)",
+    "title" : "coffee distributor",
+    "text" : "page 9",
+    "ahref" : "page9.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page10.png)",
+    "title" : "coffee needle distributor",
+    "text" : "page 10",
+    "ahref" : "page10.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page11.png)",
+    "title" : "knock box",
+    "text" : "page 11",
+    "ahref" : "page11.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page12.png)",
+    "title" : "knock box",
+    "text" : "page 12",
+    "ahref" : "page12.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page14.png)",
+    "title" : "coffee brush",
+    "text" : "page 14",
+    "ahref" : "page14.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page15.png)",
+    "title" : "cup rinser",
+    "text" : "page 15",
+    "ahref" : "page15.html"
+  },  
+  { 
+    "img" : "url(img/shopmenu/page16.png)",
+    "title" : "shot glass",
+    "text" : "page 16",
+    "ahref" : "page16.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page17.png)",
+    "title" : "french press cold brew pot dutch coffee maker",
+    "text" : "page 17",
+    "ahref" : "page17.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page18.png)",
+    "title" : "portafilters",
+    "text" : "page 18",
+    "ahref" : "page18.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page19.png)",
+    "title" : "coffee scale",
+    "text" : "page 19",
+    "ahref" : "page19.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page20.png)",
+    "title" : "dosing funnel",
+    "text" : "page 20",
+    "ahref" : "page20.html"
+  }, 
+  { 
+    "img" : "url(img/shopmenu/page21.png)",
+    "title" : "coffee filter",
+    "text" : "page 21",
+    "ahref" : "page21.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page22.png)",
+    "title" : "cup server",
+    "text" : "page 22",
+    "ahref" : "page22.html"
+  },  
+  { 
+    "img" : "url(img/shopmenu/page23.png)",
+    "title" : "accessories for italian coffee",
+    "text" : "page 23",
+    "ahref" : "page23.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page24.png)",
+    "title" : "tamping station",
+    "text" : "page 24",
+    "ahref" : "page24.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page25.png)",
+    "title" : "coffee grinder",
+    "text" : "page 25",
+    "ahref" : "page25.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page26.png)",
+    "title" : "daily coffeeware",
+    "text" : "page 26",
+    "ahref" : "page26.html"
+  },
+  { 
+    "img" : "url(img/shopmenu/page27.png)",
+    "title" : "barista gift",
+    "text" : "page 27",
+    "ahref" : "page27.html"
+  }  
+]`
